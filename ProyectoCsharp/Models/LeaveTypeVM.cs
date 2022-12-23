a60b38678045baeec0b6aaf907ed73521955e71b
AlbeiroFranco <jose.arleck91@hotmail.com>
2022-12-22 23:18:11 -0500

    Feat: controllers

--- ProyectoCsharp/Models/LeaveTypeVM.cs
+++ ProyectoCsharp/Models/LeaveTypeVM.cs
@@ -1,6 +1,14 @@
-﻿namespace ProyectoCsharp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoCsharp.Models
 {
     public class LeaveTypeVM
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name = "Default number of days")]
+        public int DefaultDays { get; set; }
+
     }
 }
